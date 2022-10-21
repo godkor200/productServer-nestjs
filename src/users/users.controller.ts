8b985323a0a7bef1b77b2f06909935ef63275b5f
godkor200 <godkor200@gmail.com>
2022-10-21 18:01:49 +0900

    기능 : 스웨거 설명 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,11 +3,9 @@ import {
   Get,
   Post,
   Body,
-  Res,
-  Patch,
   Param,
   Delete,
-  HttpStatus,
+  Put,
 } from '@nestjs/common';
 import { UsersService } from '@users/users.service';
 import { CreateUserDto } from '@users/dto/create-user.dto';
@@ -15,8 +13,8 @@ import { UpdateUserDto } from '@users/dto/update-user.dto';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from '@schema/user.schema';
 
-@ApiTags('관리자 API')
 @Controller('v1/users')
+@ApiTags('관리자 API')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
@@ -31,22 +29,53 @@ export class UsersController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: '관리자 찾기 API',
+    description: '전체 유저 불러오기',
+  })
+  @ApiCreatedResponse({
+    description: '관리자를 조회한다.',
+    type: [User],
+  })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: '관리자 찾기 API',
+    description: '유저 불러오기',
+  })
+  @ApiCreatedResponse({
+    description: '하나의 관리자를 조회한다.',
+    type: User,
+  })
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
+  @ApiOperation({
+    summary: '관리자 업데이트 API',
+    description: '유저 업데이트',
+  })
+  @ApiCreatedResponse({
+    description: '하나의 관리자를 업데이트 한다.',
+    type: User,
+  })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: '관리자 삭제 API',
+    description: '유저 삭제',
+  })
+  @ApiCreatedResponse({
+    description: '하나의 관리자를 삭제 한다.',
+  })
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(id);
   }
 }
